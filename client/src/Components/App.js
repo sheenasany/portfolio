df42645d276dbb863e30443218f5258dfac247f7
Sheena Sang <sheenasany@gmail.com>
2023-04-01 18:15:40 -0400

    created route for About component

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,6 +1,6 @@
 // import { useState, useEffect } from "react";
 import {Routes, Route} from "react-router-dom";
-
+import About from "./About";
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-
+        <Route path="/about" element={<About />}/>
       </Routes>
     </div>
   );
