1d26efae64330482ec18ce31139ad307df7f64a0
Sheena Sang <sheenasany@gmail.com>
2023-04-01 18:49:06 -0400

    Added NavBar component and Home/About Me links

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,12 +2,14 @@
 import {Routes, Route} from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import NavBar from "./NavBar";
 
 function App() {
 
 
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom"
+
+function NavBar() {
+    return(
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/about">About Me</Link>
+        </div>
+    )
+}
+
+export default NavBar
