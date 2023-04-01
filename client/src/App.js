83feb9269193f438a2adf8d158d86b698f51b230
Sheena Sang <sheenasany@gmail.com>
2023-04-01 17:57:33 -0400

    installed ract router dom and created About component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import {Routes, Route} from "react-router-dom";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <Routes>
+
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Components/About.js
@@ -0,0 +1,9 @@
+function About(){
+    return(
+        <div>
+            <h1 style={{fontWeight: "60px"}}>This element could not contain me! I am here to stay!</h1>
+        </div>
+    )
+}
+
+export default About
