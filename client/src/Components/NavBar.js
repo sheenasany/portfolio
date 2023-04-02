85fbc009cbee5329b93d819fd852e0921002f6e8
Sheena Sang <sheenasany@gmail.com>
2023-04-01 20:59:26 -0400

    css to navbar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -3,9 +3,11 @@ import {Link} from "react-router-dom"
 function NavBar() {
     return(
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About Me</Link>
-            <Link to="/contact">Contact</Link>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
         </div>
     )
 }
