7efa6cc37e521a432ed460de7073df6e8a59da24
Sheena Sang <sheenasany@gmail.com>
2023-04-09 07:08:10 -0400

    added images for music fest app

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -15,7 +15,6 @@ function Projects(){
                     <img src="" alt="" />
                     <p>Users can save their favorite songs to a playlist, can check whether their favorite bands are touring, and add additional songs. Collaborated with Elliot Brown on this project.</p>
                     <a href="https://github.com/elilogbro/Playlist-App" target="_blank" rel="noreferrer">Github</a>
-
             </ul>
         </div>
     )
