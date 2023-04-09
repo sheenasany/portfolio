35e234e1097cb3cdae2550f7490ad30fba935daf
Sheena Sang <sheenasany@gmail.com>
2023-04-09 01:07:31 -0400

    added 1 project with links, and fixed links in contact component

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -3,15 +3,15 @@ function Contact() {
         <div>
             <h2>Feel free to contact me here</h2>
             <br/>
-            <a href={"mailto: sheenasany@gmail.com"}>Email me: sheenasany@gmail.com</a>
+            <a href={"mailto: sheenasany@gmail.com"} target="_blank" rel="noreferrer">Email me: sheenasany@gmail.com</a>
             <br/>
-            <a href="http://www.calendly.com/sheenasang">Schedule a time to chat</a>
+            <a href="http://www.calendly.com/sheenasang" target="_blank" rel="noreferrer">Schedule a time to chat</a>
             <br/>
-            <a href="https://www.linkedin.com/in/sheena-sang/">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/sheena-sang/" target="_blank" rel="noreferrer">LinkedIn</a>
             <br/>
-            <a href="http://github.com/sheenasany">GitHub</a>
+            <a href="http://github.com/sheenasany" target="_blank" rel="noreferrer">GitHub</a>
             <br/>
-            <a href="http://dev.to/sheenasany">Blog</a>
+            <a href="http://dev.to/sheenasany" target="_blank" rel="noreferrer">Blog</a>
         </div>
     )
 }
--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -1,7 +1,15 @@
 function Projects(){
     return(
         <div>
-            <h1>Projects will go here.</h1>
+            <h1>Projects</h1>
+            <ul>
+                <li>Music Festival App</li>
+                <img src="" alt="" />
+                <p>Music Festival Planner that allows users to budget, plan, and organize the festivals they will be attending.</p>
+                <a href="https://www.loom.com/share/7df537bd4c84442ea19003ed88d038bb" target="_blank" rel="noreferrer">Demo</a>
+                <br/>
+                <a href="https://github.com/sheenasany/music-festival-planner" target="_blank" rel="noreferrer">Github</a>
+            </ul>
         </div>
     )
 }
