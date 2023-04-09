982297b9623801070b6879754f47ffd138cf6324
Sheena Sang <sheenasany@gmail.com>
2023-04-09 06:07:43 -0400

    added playlist app project to projects component

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -4,11 +4,18 @@ function Projects(){
             <h1>Projects</h1>
             <ul>
                 <li>Music Festival App</li>
-                <img src="" alt="" />
-                <p>Music Festival Planner that allows users to budget, plan, and organize the festivals they will be attending.</p>
-                <a href="https://www.loom.com/share/7df537bd4c84442ea19003ed88d038bb" target="_blank" rel="noreferrer">Demo</a>
+                    <img src="" alt="" />
+                    <p>Music Festival Planner that allows users to budget, plan, and organize the festivals they will be attending.</p>
+                    <a href="https://www.loom.com/share/7df537bd4c84442ea19003ed88d038bb" target="_blank" rel="noreferrer">Demo</a>
+                    <br/>
+                    <a href="https://github.com/sheenasany/music-festival-planner" target="_blank" rel="noreferrer">Github</a>
                 <br/>
-                <a href="https://github.com/sheenasany/music-festival-planner" target="_blank" rel="noreferrer">Github</a>
+
+                <li>Playlist App</li>
+                    <img src="" alt="" />
+                    <p>Users can save their favorite songs to a playlist, can check whether their favorite bands are touring, and add additional songs. Collaborated with Elliot Brown on this project.</p>
+                    <a href="https://github.com/elilogbro/Playlist-App" target="_blank" rel="noreferrer">Github</a>
+
             </ul>
         </div>
     )
