dc22c1f724115bc4e03d97c014466652294c9f8d
Sheena Sang <sheenasany@gmail.com>
2023-04-09 07:33:46 -0400

    renamed images and created SliderData component

---
+++ client/src/Components/SliderData.js
@@ -0,0 +1,12 @@
+export const SliderData = [
+    { image: "client/images/Homepage.png" },
+    { image: "client/images/Login.png" },
+    { image: "client/images/Signup.png" },
+    { image: "client/images/Festival_List.png" },
+    { image: "client/images/Festival_Display.png" },
+    { image: "client/images/Add_Festival_to_Planner.png" },
+    { image: "client/images/Festival_Map.png"},
+    { image: "client/images/Festival_Map_Display.png"},
+    { image: "client/images/Festival_Planner.png"},
+    { image: "client/images/Edit_Festival.png"},
+]
